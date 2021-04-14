3a58e1adc23a9afd00316616f72531f9712d4b3e
NickStrupat <nick.strupat@gmail.com>
2021-04-14 00:07:28 -0400

    fixed simple version

--- Program.cs
+++ Program.cs
@@ -1,140 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
-using Spectre.Console;
 
-var sourceText = "5 + 4a = 9";//"1 + 23 / 3 * 3.14 = 4";
+var sourceText = "1 + 23 / 3 * 3.14 = 4";
 var chars = CharStreams.fromString(sourceText);
 var lexer = new CalculatorLexer(chars);
 var tokens = new CommonTokenStream(lexer);
 var parser = new CalculatorParser(tokens) { BuildParseTree = false };
-var listener = new TreePrinterListener(parser);
+var listener = new TreePrinterListener(parser.RuleNames);
 parser.AddParseListener(listener);
 parser.equation();
 
-//ParseTreeWalker.Default.Walk(listener, parser.equation());
-AnsiConsole.Render(listener.Tree);
-
-//Console.WriteLine(parser.equation().ToStringTree());
-//parser.Reset();
-
-//parser.PrintPrettyTree();
-//new PrettyTreePrinterVisitor().Visit(parser.equation());
-
-static class ParseTreeExtensions
-{
-	public static void PrintPrettyTree(this Parser parser, String ruleColor = "green", String terminalColor = "red")
-	{
-		var startRuleMethod = parser.GetType().GetMethod(parser.RuleNames[0]);
-		var startRule = (ParserRuleContext)startRuleMethod.Invoke(parser, null);
-		var tree = new Tree("")
-			.Style("grey");
-		Visit(startRule, tree);
-		AnsiConsole.Render(tree);
-
-		void Visit(IParseTree parseTree, IHasTreeNodes node)
-		{
-			if (parseTree is ITerminalNode tn)
-				node.AddNode($"[{terminalColor}]{tn.Symbol.Text}[/]");
-			else if (parseTree is ParserRuleContext prc)
-			{
-				//if (prc.ChildCount == 1 && prc.children[0] is )
-				node = node.AddNode($"[{ruleColor}]{parser.RuleNames[prc.RuleIndex]}[/]");
-				foreach (var child in prc.children)
-					Visit(child, node);
-			}
-			else //if (parseTree is ErrorNodeImpl en)
-				throw new Exception(parseTree.GetType().Name);
-		}
-	}
-}
-
 class TreePrinterListener : IParseTreeListener
 {
-	readonly Parser parser;
-	readonly String ruleColor;
-	readonly String terminalColor;
-	public TreePrinterListener(Parser parser, String ruleColor = "green", String terminalColor = "red") =>
-		(this.parser, this.ruleColor, this.terminalColor) = (parser, ruleColor, terminalColor);
+	readonly String[] ruleNames;
+	readonly ConsoleColor ruleColor;
+	readonly ConsoleColor terminalColor;
 
-	public Tree Tree { get; private set; }
-	
-	readonly Stack<IHasTreeNodes> nodeStack = new();
-	readonly Stack<String> ruleNameStack = new();
-	readonly List<String> terminals = new();
+	public TreePrinterListener(
+		String[] ruleNames,
+		ConsoleColor ruleColor = ConsoleColor.Green,
+		ConsoleColor terminalColor = ConsoleColor.Red
+	)
+	{
+		this.ruleNames = ruleNames;
+		this.ruleColor = ruleColor;
+		this.terminalColor = terminalColor;
+	}
+
+	Byte depth;
+
+	void WritePadding() => Console.Write(new String(' ', depth * 3));
 
 	public void EnterEveryRule(ParserRuleContext ctx)
 	{
-		var ruleName = $"[{ruleColor}]{parser.RuleNames[ctx.RuleIndex]}[/]";
-		if (Tree == null)
-		{
-			Tree = new Tree(ruleName);
-			nodeStack.Push(Tree);
-			return;
-		}
-		ruleNameStack.Push(parser.RuleNames[ctx.RuleIndex]);
-		// nodeStack.Push(nodeStack.Peek().AddNode(ruleName));
-		// foreach (var token in ctx.children.OfType<ITerminalNode>())
-		// 	nodeStack.Peek().AddNodes($"[red]{token.Symbol.Text}[/]");
+		WritePadding();
+		using (new ConsoleColorizer(ruleColor))
+			Console.WriteLine(ruleNames[ctx.RuleIndex]);
+		depth++;
 	}
 
-	public void ExitEveryRule(ParserRuleContext ctx)
-	{
-		if (!terminals.Any())
-			return;
-		nodeStack.Pop().AddNodes(terminals);
-		terminals.Clear();
-	}
+	public void ExitEveryRule(ParserRuleContext ctx) => depth--;
 
 	public void VisitErrorNode(IErrorNode node)
 	{
-		AddNode();
-		terminals.Add($"[black on red]{node.GetText()}[/]");
+		depth++;
+		WritePadding();
+		using (new ConsoleColorizer(ConsoleColor.Black, ConsoleColor.Red))
+			Console.WriteLine(node.GetText());
+		depth--;
 	}
 
 	public void VisitTerminal(ITerminalNode node)
 	{
-		AddNode();
-		terminals.Add($"[{terminalColor}]{node.GetText()}[/]");
+		depth++;
+		WritePadding();
+		using (new ConsoleColorizer(terminalColor))
+			Console.WriteLine(node.GetText());
+		depth--;
 	}
 
-	private void AddNode()
+	ref struct ConsoleColorizer
 	{
-		var text = String.Join(" -> ", ruleNameStack.Reverse().Select(x => $"[{ruleColor}]{x}[/]"));
-		ruleNameStack.Clear();
-		nodeStack.Push(nodeStack.Peek().AddNode(text));
+		readonly ConsoleColor background;
+		readonly ConsoleColor foreground;
+
+		public ConsoleColorizer(ConsoleColor foreground, ConsoleColor? background = null)
+		{
+			this.background = Console.BackgroundColor;
+			this.foreground = Console.ForegroundColor;
+			Console.ForegroundColor = foreground;
+			Console.BackgroundColor = background ?? Console.BackgroundColor;
+		}
+
+		public void Dispose()
+		{
+			Console.BackgroundColor = background;
+			Console.ForegroundColor = foreground;
+		}
 	}
 }
-
-// class PrettyTreePrinterVisitor : IParseTreeVisitor<Object>
-// {
-// 	public Tree Tree { get; private set; }
-// 	private IHasTreeNodes node;
-// 	public Object Visit(IParseTree tree)
-// 	{
-// 		if (Tree == null)
-// 		{
-// 			Tree = new Tree(tree);
-// 		}
-// 		Console.WriteLine(tree.GetText());
-// 		return null;
-// 	}
-//
-// 	public Object VisitChildren(IRuleNode node)
-// 	{
-// 		return null;
-// 	}
-//
-// 	public Object VisitErrorNode(IErrorNode node)
-// 	{
-// 		return null;
-// 	}
-//
-// 	public Object VisitTerminal(ITerminalNode node)
-// 	{
-// 		return null;
-// 	}
-// }
